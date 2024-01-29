0abb82984f893a7184a936a7e5c8e6b31ea8c64a
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-01-29 04:26:33 +0000

    added new tag to indicate if a tool is new

--- src/sections/tools/index.tsx
+++ src/sections/tools/index.tsx
@@ -1,9 +1,9 @@
+import Navigator from "@components/navigator"
 import Section from "@components/section"
+import NavigationProvider from "@providers/navigation-provider"
 import Comments from "./comments"
 import { DATA } from "./data"
 import Item from "./item"
-import NavigationProvider from "@providers/navigation-provider"
-import Navigator from "@components/navigator"
 
 interface Props {
     id: number
@@ -20,14 +20,8 @@ export default function Tools({ id }: Props) {
                 <h1 className="text-5xl font-bold">Tools</h1>
                 <NavigationProvider data={DATA} duration={10}>
                     <div className="flex-1 flex flex-wrap gap-6">
-                        {DATA.map(({ image, meta: { id, name, link } }) => (
-                            <Item
-                                key={id}
-                                id={id}
-                                name={name}
-                                link={link}
-                                image={image}
-                            />
+                        {DATA.map(({ image, meta }) => (
+                            <Item key={id} image={image} meta={meta} />
                         ))}
                     </div>
                     <div className="hidden sm:block h-1" />
