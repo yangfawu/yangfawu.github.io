7897ea36097b942855f0924e70ff2fffa1da381e
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-01-29 04:49:53 +0000

    added a feature where NavigationProvider will not progress automatically unless section is visible

--- src/sections/tools/index.tsx
+++ src/sections/tools/index.tsx
@@ -1,6 +1,7 @@
 import Navigator from "@components/navigator"
 import Section from "@components/section"
 import NavigationProvider from "@providers/navigation-provider"
+import { useRef } from "react"
 import Comments from "./comments"
 import { DATA } from "./data"
 import Item from "./item"
@@ -9,6 +10,8 @@ interface Props {
     id: number
 }
 export default function Tools({ id }: Props) {
+    const ref = useRef<any>(null)
+
     return (
         <Section
             id={id}
@@ -16,9 +19,9 @@ export default function Tools({ id }: Props) {
             anchor="tools"
             background="bg-slate-300"
         >
-            <div className="space-y-8">
+            <div className="space-y-8" ref={ref}>
                 <h1 className="text-5xl font-bold">Tools</h1>
-                <NavigationProvider data={DATA} duration={10}>
+                <NavigationProvider targetRef={ref} data={DATA} duration={10}>
                     <div className="flex-1 flex flex-wrap gap-6">
                         {DATA.map(({ image, meta }) => (
                             <Item key={meta.id} image={image} meta={meta} />
